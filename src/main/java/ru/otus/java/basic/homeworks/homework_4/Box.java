d7effa96f5abb0758262cc975363623f887d24fc
StepinIvan <ivan.stepin.99@mail.ru>
2024-11-30 13:13:17 +0700

    Homework_4 looks like ready, need to review.

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework_4/Box.java
@@ -0,0 +1,65 @@
+package ru.otus.java.basic.homeworks.homework_4;
+
+public class Box {
+    private int size;
+    private String color;
+    private boolean isOpened;
+    private boolean isFilled;
+
+    public Box(int size, String color, boolean isOpened) {
+        this.size = size;
+        this.color = color;
+        this.isOpened = isOpened;
+        this.isFilled = false;
+    }
+
+    public void open() {
+        isOpened = true;
+        System.out.println("Коробка открыта");
+    }
+
+    public void close() {
+        isOpened = false;
+        System.out.println("Коробка закрыта");
+    }
+
+    public void changeColor(String color) {
+        this.color = color;
+        System.out.println("Коробка перекрашена в " + color + " цвет");
+    }
+
+    public void info() {
+        System.out.println("Размер коробки: " + size + "\nЦвет коробки: " + color);
+        if (isOpened) {
+            System.out.println("Статус коробки: открыта");
+        } else {
+            System.out.println("Статус коробки: закрыта");
+        }
+    }
+
+    public void putItem(String item) {
+        if (isOpened) {
+            if (isFilled) {
+                System.out.println("В коробке уже есть предмет");
+            } else {
+                isFilled = true;
+                System.out.println("Вы положили " + item + " в коробку");
+            }
+        } else {
+            System.out.println("Откройте коробку для взаимодействия с ее предметами");
+        }
+    }
+
+    public void throwAwayItem() {
+        if (isOpened) {
+            if (isFilled) {
+                System.out.println("Вы выбросили предмет из коробки");
+                isFilled = false;
+            } else {
+                System.out.println("Вы не можете ничего выбросить, так как в коробке ничего нет");
+            }
+        } else {
+            System.out.println("Откройте коробку для взаимодействия с ее предметами");
+        }
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework_4/Main.java
@@ -0,0 +1,27 @@
+package ru.otus.java.basic.homeworks.homework_4;
+
+import java.time.Year;
+
+public class Main {
+    public static void main(String[] args) {
+        int currentYear = Year.now().getValue();
+        User[] listOfUsers = {
+                new User ("Иванов", "Никита", "Петрович", 1992, "petrovich@mail.ru"),
+                new User ("Петров", "Денис", "Альбертович", 1984, "deniska@mail.ru"),
+                new User ("Сидорова", "Светлана", "Ивановна", 1998, "sidSvet@mail.ru"),
+                new User ("Малюта", "Иван", "Рудольфович", 1995, "MalJohn@mail.ru"),
+                new User ("Юдин", "Василий", "Петрович", 2002, "proger2000@mail.ru"),
+                new User ("Непейвода", "Арина", "Эдуардовна", 2005, "engineer5@mail.ru"),
+                new User ("Макаренко", "Елизавета", "Андреевна", 2001, "makElisaveta@mail.ru"),
+                new User ("Федькин", "Никита", "Сергеевич", 1978, "NikFed@mail.ru"),
+                new User ("Васильева", "Елена", "Олеговна", 1988, "atom235@mail.ru"),
+                new User ("Крутов", "Михаил", "Алексеевич", 1981, "physics@mail.ru"),
+        };
+        for (User user: listOfUsers){
+            if (currentYear - user.getBirthYear() > 40) {
+                user.info();
+                System.out.println("------------------------------");
+            }
+        }
+    }
+}
