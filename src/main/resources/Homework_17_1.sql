34448e04ceb12cba917f89a4ed2895bedb784c50
StepinIvan <ivan.stepin.99@mail.ru>
2025-01-22 19:26:52 +0700

    Homework 17 fixes.

--- src/main/resources/Homework_17_1.sql
+++ src/main/resources/Homework_17_1.sql
@@ -15,21 +15,3 @@ CREATE TABLE answers (
  answer_content text NOT NULL,
  is_right_answer BOOLEAN NOT NULL
 );
-
-INSERT INTO tests (test_name) VALUES ('Тест по математике');
-
-INSERT INTO questions (test_id, question_content) VALUES 
-(1, '2 + 2 = ?'),
-(1, '16 / 4 = ?'),
-(1, '25 * 4 = ?');
-
-INSERT INTO answers (question_id, answer_content, is_right_answer) VALUES 
-(1, '3', FALSE),
-(1, '5', FALSE),
-(1, '10', FALSE),
-(1, '4', TRUE),
-(2, '4', TRUE),
-(2, '2', FALSE),
-(2, '8', FALSE),
-(3, '80', FALSE),
-(3, '100', TRUE);
