2ba9671231cffa36e34c31ae7a2294aba99be493
StepinIvan <ivan.stepin.99@mail.ru>
2025-01-25 19:23:06 +0700

    fixes

--- homeworks/src/main/java/ru/otus/java/basic/homeworks/homework_19/Apple.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.java.basic.homeworks.homework_19;
-
-
-public class Apple extends Fruit{
-    public Apple(double weight) {
-        super(weight);
-    }
-}
--- homeworks/src/main/java/ru/otus/java/basic/homeworks/homework_19/Orange.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.homeworks.homework_19;
-
-public class Orange extends Fruit{
-    public Orange(double weight) {
-        super(weight);
-    }
-}
