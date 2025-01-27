3af020e0d737b373f499b90e31db06082ff642d5
StepinIvan <ivan.stepin.99@mail.ru>
2025-01-27 20:40:00 +0700

    Homework 22 methods ready.

---
+++ homeworks/src/main/java/ru/otus/java/basic/homeworks/homework_22/Main.java
@@ -0,0 +1,45 @@
+package ru.otus.java.basic.homeworks.homework_22;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] testArray = new int[] {1,2,3,4,5,1,2,2,3};
+        int[] testArray2 = new int[] {1,2,2,1};
+        System.out.println(Arrays.toString(arrayAfterLastOne(testArray)));
+        System.out.println(checkArrayFilling(testArray2));
+    }
+    public static int[] arrayAfterLastOne(int[] inArray) {
+        int oneIndex = -1;
+        for (int i = 0; i < inArray.length; i++) {
+            if (inArray[i] == 1) {
+                oneIndex = i;
+            }
+        }
+        if (oneIndex == -1) {
+            throw new RuntimeException("Во входном массиве отсутствуют единицы");
+        }
+        int[] newArray = new int[inArray.length - oneIndex];
+        for (int i = oneIndex; i < inArray.length; i++) {
+            newArray[i - oneIndex] = inArray[i];
+        }
+        return newArray;
+    }
+    public static boolean checkArrayFilling(int[] array) {
+        int oneCount = 0;
+        int twoCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                oneCount += 1;
+            } else if (array[i] == 2) {
+                twoCount += 1;
+            } else {
+                return false;
+            }
+        }
+        if (oneCount == 0 || twoCount == 0) {
+            return false;
+        }
+        return true;
+    }
+}
