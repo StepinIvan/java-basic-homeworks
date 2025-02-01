ce16f982b23705f818982072014b63bd846b8ce7
StepinIvan <ivan.stepin.99@mail.ru>
2025-02-01 11:57:54 +0700

    Homework 22 ready.

--- homeworks/src/main/java/ru/otus/java/basic/homeworks/homework_22/Main.java
+++ homeworks/src/main/java/ru/otus/java/basic/homeworks/homework_22/Main.java
@@ -1,13 +1,18 @@
 package ru.otus.java.basic.homeworks.homework_22;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Arrays;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         int[] testArray = new int[] {1,2,3,4,5,1,2,2,3};
         int[] testArray2 = new int[] {1,2,2,1};
-        System.out.println(Arrays.toString(arrayAfterLastOne(testArray)));
-        System.out.println(checkArrayFilling(testArray2));
+        LOGGER.info(String.format("Результат расчета метода arrayAfterLastOne с входным массивом: %s - %s",
+                Arrays.toString(testArray),Arrays.toString(arrayAfterLastOne(testArray))));
+        LOGGER.info(String.format("Результат расчета метода checkArrayFilling с входным массивом: %s - %s",
+                Arrays.toString(testArray2),checkArrayFilling(testArray2)));
     }
     public static int[] arrayAfterLastOne(int[] inArray) {
         int oneIndex = -1;
@@ -17,7 +22,8 @@ public class Main {
             }
         }
         if (oneIndex == -1) {
-            throw new RuntimeException("Во входном массиве отсутствуют единицы");
+            LOGGER.error("Ошибка! Во входном массиве отсутствуют единицы");
+            throw new RuntimeException();
         }
         int[] newArray = new int[inArray.length - oneIndex - 1];
         for (int i = oneIndex; i < inArray.length - 1; i++) {
